900f7e809933e0adfee075b6e48cfe18f096346a
Marco Garlappi <marcogarlappi@italymail.biz>
2026-01-21 10:06:23 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,47 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self,
+                 comfort_class: int, clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car: Car) -> float:
+        return (round((car.comfort_class
+                       * (self.clean_power
+                          - car.clean_mark)
+                       * self.average_rating
+                       / self.distance_from_city_center), 1))
+
+    def wash_single_car(self, car: Car) -> float:
+        single_washing_price = 0.0
+        if self.clean_power > car.clean_mark:
+            single_washing_price = self.calculate_washing_price(car)
+            car.clean_mark = self.clean_power
+        return single_washing_price
+
+    def serve_cars(self, cars: list[Car]) -> float:
+        total_income = 0.0
+        for car in cars:
+            if self.clean_power > car.clean_mark:
+                single_wash_price = self.wash_single_car(car)
+                total_income += single_wash_price
+        return round(total_income, 1)
+
+    def rate_service(self, new_rating: float) -> None:
+        self.average_rating = (round((self.average_rating
+                                      * self.count_of_ratings
+                                      + new_rating)
+                                     / (self.count_of_ratings + 1), 1))
+        self.count_of_ratings += 1
